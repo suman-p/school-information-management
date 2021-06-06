8cb2dd0822a28d7de3af23b2acce70126a0e80cc
suman.pallapati <suman.pallapati@yahoo.com>
2021-06-06 13:24:51 +0530

    Initial Commit

---
+++ src/main/java/com/rest/sim/SchoolInformationManagement/SchoolInformationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.rest.sim.SchoolInformationManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolInformationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolInformationManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rest/sim/SchoolInformationManagement/SchoolInformationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.sim.SchoolInformationManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolInformationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
